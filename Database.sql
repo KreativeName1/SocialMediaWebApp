271f286b5eac049f4692e1e2b3e963189543666d
KreativeName <115575690+KreativeName@users.noreply.github.com>
2024-03-20 23:31:28 +0100

    Verifizierung in die Datenbank hinzugefügt

--- Database.sql
+++ Database.sql
@@ -8,5 +8,9 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `image` varchar(255) DEFAULT NULL
+  `image` varchar(255) DEFAULT NULL,
+  `verified` boolean NOT NULL DEFAULT FALSE,
+  `verification_code` varchar(255) DEFAULT NULL,
+  `verification_code_expiration` timestamp DEFAULT NULL,
+  `verfied_at` timestamp DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
